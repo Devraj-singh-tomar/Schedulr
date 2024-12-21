d863b0fd4c0837827b53ed655247a9577e39e007
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-12-21 13:31:57 +0530

    add: availability page and prisma schema

--- app/actions.ts
+++ app/actions.ts
@@ -1,14 +1,11 @@
 "use server";
 
 import { parseWithZod } from "@conform-to/zod";
+import { redirect } from "next/navigation";
 import prisma from "./lib/db";
 import { requireUser } from "./lib/hooks";
-import {
-  onboardingSchema,
-  onboardingSchemaValidation,
-  settingsSchema,
-} from "./lib/zodSchemas";
-import { redirect } from "next/navigation";
+import { onboardingSchemaValidation, settingsSchema } from "./lib/zodSchemas";
+import { revalidatePath } from "next/cache";
 
 export async function OnboardingAction(prevState: any, formdata: FormData) {
   const session = await requireUser();
@@ -41,6 +38,47 @@ export async function OnboardingAction(prevState: any, formdata: FormData) {
     data: {
       userName: submission.value.userName,
       name: submission.value.fullName,
+      availability: {
+        createMany: {
+          data: [
+            {
+              day: "Monday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Tuesday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Wednesday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Thursday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Friday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Saturday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Sunday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+          ],
+        },
+      },
     },
   });
 
@@ -69,4 +107,45 @@ export async function SettingsAction(prevState: any, formData: FormData) {
   });
 
   return redirect("/dashboard");
+}
+
+export async function updateAvailabilityAction(formData: FormData) {
+  const session = await requireUser();
+
+  const rawData = Object.fromEntries(formData.entries());
+
+  const availabilityData = Object.keys(rawData)
+    .filter((key) => key.startsWith("id-"))
+    .map((key) => {
+      const id = key.replace("id-", "");
+
+      return {
+        id,
+        isActive: rawData[`isActive-${id}`] === "on",
+        fromTime: rawData[`fromTime-${id}`] as string,
+        tillTime: rawData[`tillTime-${id}`] as string,
+      };
+    });
+
+  try {
+    await prisma.$transaction(
+      availabilityData.map((item) =>
+        prisma.availability.update({
+          where: {
+            id: item.id,
+          },
+
+          data: {
+            isActive: item.isActive,
+            fromTime: item.fromTime,
+            tillTime: item.tillTime,
+          },
+        })
+      )
+    );
+
+    revalidatePath("/dashboard/availability");
+  } catch (error) {
+    console.error(error);
+  }
 }
---
+++ app/dashboard/availability/page.tsx
@@ -0,0 +1,112 @@
+import { updateAvailabilityAction } from "@/app/actions";
+import prisma from "@/app/lib/db";
+import { requireUser } from "@/app/lib/hooks";
+import { times } from "@/app/lib/times";
+import { SubmitButton } from "@/components/SubmitButtons";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
+import { notFound } from "next/navigation";
+import React from "react";
+
+const getData = async (userId: string) => {
+  const data = await prisma.availability.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+
+  if (!data) {
+    return notFound();
+  }
+
+  return data;
+};
+
+const AvailabilityRoute = async () => {
+  const session = await requireUser();
+  const data = await getData(session.user?.id as string);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Availability</CardTitle>
+        <CardDescription>
+          In this section you can manage your availability.
+        </CardDescription>
+      </CardHeader>
+
+      <form action={updateAvailabilityAction}>
+        <CardContent className="flex flex-col gap-y-2">
+          {data.map((item) => (
+            <div
+              className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center gap-4"
+              key={item.id}
+            >
+              <input type="hidden" name={`id-${item.id}`} value={item.id} />
+
+              <div className="flex items-center gap-x-3">
+                <Switch
+                  name={`isActive-${item.id}`}
+                  defaultChecked={item.isActive}
+                />
+                <p className="sm:font-semibold">{item.day}</p>
+              </div>
+
+              <Select name={`fromTime-${item.id}`} defaultValue={item.fromTime}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="From" />
+                </SelectTrigger>
+
+                <SelectContent>
+                  <SelectGroup>
+                    {times.map((time) => (
+                      <SelectItem key={time.id} value={time.time}>
+                        {time.time}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <Select name={`tillTime-${item.id}`} defaultValue={item.tillTime}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Till" />
+                </SelectTrigger>
+
+                <SelectContent>
+                  <SelectGroup>
+                    {times.map((time) => (
+                      <SelectItem key={time.id} value={time.time}>
+                        {time.time}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          ))}
+        </CardContent>
+
+        <CardFooter>
+          <SubmitButton text="Save Changes" />
+        </CardFooter>
+      </form>
+    </Card>
+  );
+};
+export default AvailabilityRoute;
