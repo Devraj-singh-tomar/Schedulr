dc39cf821b84bab55ceb9be95c3d6f1ef766d542
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-12-26 20:40:49 +0530

    created meeting page

---
+++ app/dashboard/meetings/page.tsx
@@ -0,0 +1,118 @@
+import { cancelMeetingAction } from "@/app/actions";
+import prisma from "@/app/lib/db";
+import { requireUser } from "@/app/lib/hooks";
+import { nylas } from "@/app/lib/nylas";
+import { EmptyState } from "@/components/EmptyState";
+import { SubmitButton } from "@/components/SubmitButtons";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { format, fromUnixTime } from "date-fns";
+import { VideoIcon } from "lucide-react";
+
+async function getData(userId: string) {
+  const userData = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+
+    select: {
+      grantId: true,
+      grantEmail: true,
+    },
+  });
+
+  if (!userData) {
+    throw new Error("User not found");
+  }
+
+  const data = await nylas.events.list({
+    identifier: userData?.grantId as string,
+    queryParams: {
+      calendarId: userData?.grantEmail as string,
+    },
+  });
+
+  return data;
+}
+
+const Meetings = async () => {
+  const session = await requireUser();
+  const data = await getData(session.user?.id as string);
+
+  return (
+    <>
+      {data.data.length < 1 ? (
+        <EmptyState
+          title="No meeting found"
+          description="you don't have any meetings yet"
+          buttonText="create a new event"
+          href="/dashboard/new"
+        />
+      ) : (
+        <Card>
+          <CardHeader>
+            <CardTitle>Meetings</CardTitle>
+            <CardDescription>
+              See upcoming and past events booked through your event type links.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            {data.data.map((item) => (
+              <form key={item.id} action={cancelMeetingAction}>
+                <input type="hidden" name="eventId" value={item.id} />
+
+                <div className="grid grid-cols-3 justify-between items-center">
+                  <div>
+                    <p className="text-muted-foreground text-sm">
+                      {format(fromUnixTime(item.when.startTime), "EEE, dd MMM")}
+                    </p>
+
+                    <p className="text-muted-foreground text-xs pt-1">
+                      {format(fromUnixTime(item.when.startTime), "hh:mm a")} -{" "}
+                      {format(fromUnixTime(item.when.endTime), "hh:mm a")}
+                    </p>
+
+                    <div className="flex items-center mt-1">
+                      <VideoIcon className="size-4 mr-2 text-primary" />{" "}
+                      <a
+                        className="text-xs text-primary underline underline-offset-4"
+                        target="_blank"
+                        href={item.conferencing.details.url}
+                      >
+                        Join Meeting
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col items-start">
+                    <h2 className="text-sm font-medium">{item.title}</h2>
+                    <p className="text-sm text-muted-foreground">
+                      You and {item.participants[0].name}
+                    </p>
+                  </div>
+
+                  <SubmitButton
+                    text="Cancel Event"
+                    variant="destructive"
+                    className="w-fit flex ml-auto"
+                  />
+                </div>
+
+                <Separator className="my-3" />
+              </form>
+            ))}
+          </CardContent>
+        </Card>
+      )}
+    </>
+  );
+};
+
+export default Meetings;
